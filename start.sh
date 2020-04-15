e0260fc4b2cb2ed54ba76b9c74c28ed73921e36a
Valtýr Örn Kjartansson <valtyr@gmail.com>
2020-04-15 22:36:00 +0000

    use pip and python in start script

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
-pip3 install -r requirements.txt
+pip install -r requirements.txt
 
-python3 app.py
+python app.py
 
 cp model.h5 /artifacts/model.h5
 cp tokenizer.pickle /artifacts/tokenizer.pickle
